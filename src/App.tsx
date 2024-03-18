cb021e78f71120a3351019644f4cd94dbd743298
Malebo29 <shwabilegodi@gmail.com>
2024-03-18 14:19:26 +0200

    moved the code that fetches the list of shows from the API to the ShowList components. The ShowList component fetches the list of shows from the API and renders them. It uses the useEffect hook to fetch the data when the component is mounted. The App being the main component of the application, it imports the necessary components and renders them (Header and ShowList components

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,12 @@
-import { useEffect, useState } from 'react'
-import { getShows } from './api'
-import { useShowsContext } from './context/ShowsContext'
 import { Header } from './components/header/Header'
+import { ShowList } from './components/show/ShowList'
 
 function App() {
-  const { shows , setShows } = useShowsContext()
-  const [isLoading, setIsLoading] = useState(false)
-  useEffect(() => {
-    setIsLoading(true)
-    getShows('https://podcast-api.netlify.app/shows').then(data => {
-    setShows(data)
-    })
-    setIsLoading(false)
-}, [])
 
   return (
     <div>
       <Header />
-    { isLoading ? <p>Loading...</p>:shows.map((show) => <p key={show.id}>{show.title}</p>)}
+      <ShowList />
     </div>
   )
 }
