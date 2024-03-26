a3972ffa11a70fb3278193540eae97e86471c39a
Malebo29 <shwabilegodi@gmail.com>
2024-03-26 22:52:55 +0200

    The settings page and user profiles page were created, and sample code was copied from libraries as the base into these pages and then this the copied code was then edited I speak my podcast App requirement.

---
+++ src/pages/SettingsPage.tsx
@@ -0,0 +1,7 @@
+import Settings from "../components/modules/settings"
+
+export const SettingsPage = () => {
+  return (
+    <Settings />
+  )
+}
---
+++ src/pages/UserProfile.tsx
@@ -0,0 +1,8 @@
+import UserProfileForm from "../components/modules/profile";
+
+
+export const UserProfile = () => {
+  return (
+    <UserProfileForm />
+  );
+};
