44c516cfb637016eb68cc67b1eaaaa27dc7875ff
Malebo29 <shwabilegodi@gmail.com>
2024-03-27 22:28:08 +0200

    updated the FavouritesPage component to group the favourite episodes by show.
    used the reduce JS function to group the favourites by show, and then map over the grouped favourites to render the Favourite components.

--- src/components/modules/favourates/index.tsx
+++ src/components/modules/favourates/index.tsx
@@ -1,26 +1,46 @@
-import { Box, Button, CardMedia, Chip, Typography } from '@mui/material'
+import { Box, Button, CardMedia, Chip, Collapse, Typography } from '@mui/material'
 import { FavoriteProps } from '../../../utils/type'
+import { useState } from 'react'
 
 const Favourate = (episode: FavoriteProps) =>{   
+    const [readMore, setReadMore] = useState(false);
+
     return (
         <Box sx={{display:'flex', flexDirection:'column', justifyContent:'center'}}>
-            <Chip label={`Season: ${episode.seasonId}`}/>
-            <CardMedia
-                component="img"
-                image={episode.seasonImage}
-                alt={episode.seasonImage}
-                sx={{ width: '20%', height: 'auto', borderRadius: '10px', boxShadow: '0 4px 8px 0 rgba(0,0,0,0.2)' }}
-            />
-            <Typography variant='h6'>{episode.showTitle}</Typography>
-            <Typography variant='h6'>{episode.episodeTitle}</Typography>
-            <Typography variant='body2'>{episode.episodeDescription}</Typography>
-            <Box sx={{display:'flex', justifyContent:'space-between'}}>
-                <Button>Play</Button>
-                <Typography variant='body2'>{episode.lastUpdatedShowDate}</Typography>
+            <Chip label={`Season: ${episode.seasonId}`} sx={{ alignSelf: 'center' }}/>
+            <Box sx={{display:'flex', flexDirection:'row'}}>
+                <CardMedia
+                    component="img"
+                    image={episode.seasonImage}
+                    alt={episode.seasonImage}
+                    sx={{ width: '20%', height: 'auto', borderRadius: '10px', boxShadow: '0 4px 8px 0 rgba(0,0,0,0.2)' }}
+                />
             </Box>
-            <Box sx={{display:'flex', justifyContent:'space-between'}}>
+
+            <Box sx={{display:'flex', flexDirection:'column', ml: 2}}>
+                <Typography variant='h6'>{episode.showTitle}</Typography>
+                <Typography variant='h6'>{episode.episodeTitle}</Typography>
+                <Collapse in={readMore} collapsedSize={140}>
+                    <Typography variant='body2'>{episode.episodeDescription}</Typography>
+                </Collapse>
+
+                <Button onClick={() => setReadMore(!readMore)}>
+                    {readMore ? 'Read Less' : 'Read More'}
+                </Button>
+            </Box>
+
+            <Box sx={{display:'flex', justifyContent:'space-between', width: '100%'}}>
+                <Button>Play</Button>
+                <Typography variant='body2'>Updated: 
+                    {new Date(episode.lastUpdatedShowDate).toLocaleDateString('en-GB', 
+                    { year: 'numeric', month: 'long', day: 'numeric' })}</Typography>
+            </Box>
+
+            <Box sx={{display:'flex', justifyContent:'space-between', width: '100%'}}>
                 <Button>Remove</Button>
-                <Typography variant='body2'>{episode.favoredDate}</Typography>
+                <Typography variant='body2'>Favoured Date: 
+                    {new Date(episode.favoredDate).toLocaleDateString('en-GB',
+                    { year: 'numeric', month: 'long', day: 'numeric' })}</Typography>
             </Box>
         </Box>
     )
--- src/pages/FavouratesPage.tsx
+++ src/pages/FavouratesPage.tsx
@@ -1,14 +1,43 @@
-import { Container } from '@mui/material'
+import { Box, Container, Typography } from '@mui/material'
 import Favourate from '../components/modules/favourates'
 import { useShowsContext } from '../context/ShowsContext'
 import { FavoriteProps } from '../utils/type'
 
 const FavouratesPage = () => {
-  const { favourites } = useShowsContext()
+  const { favourites, sortOption } = useShowsContext()
+
+  const sortedFavourates = [...(favourites || [])].sort((a, b) => {
+    switch (sortOption) {
+      case 'title-asc': return a.showTitle.localeCompare(b.showTitle);
+      case 'title-desc': return b.showTitle.localeCompare(a.showTitle);
+
+      case 'date-asc': return new Date(a.lastUpdatedShowDate).getTime() - new Date(b.lastUpdatedShowDate).getTime();
+      case 'date-asc': return new Date(b.lastUpdatedShowDate).getTime() - new Date(a.lastUpdatedShowDate).getTime();
+
+      default:
+        return 0;
+    }
+  });
+
+  const groupedFavourates = sortedFavourates.reduce((groups, fav) => {
+    const group = (groups[fav.showTitle] || []);
+      group.push(fav);
+      groups[fav.showTitle] = group;
+      return groups;
+  }, {} as Record<string, FavoriteProps[]>);
  
   return (
     <Container>
-        {favourites?.map((fav: FavoriteProps ) => <Favourate  {...fav}/>)}
+      {groupedFavourates && Object.entries(groupedFavourates).map(([showTitle, favs]) => (
+        <Box key={showTitle}>
+          <Typography variant='h4'>{showTitle}</Typography>
+          
+          {favs.map((fav) => <Favourate key={fav.episodeId} {...fav}/>)}
+        </Box>
+      )
+      
+      )}
+        {/* {favourites?.map((fav: FavoriteProps ) => <Favourate  {...fav}/>)} */}
     </Container>
   )
 
