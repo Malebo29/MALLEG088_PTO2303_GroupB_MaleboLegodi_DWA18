4eea9ab71ba98411f49bed3fe96b5d436624b60b
Malebo29 <shwabilegodi@gmail.com>
2024-03-27 19:57:02 +0200

    Created the type for the favoured episodes for the information to be put into it and handled the state in the file that deals with the different states.
    Used the useEffect hook to fetch the users favourite podcasts from a database when the component first mounts and display right.

--- src/context/ShowsContext.tsx
+++ src/context/ShowsContext.tsx
@@ -1,5 +1,6 @@
-import { ReactNode, createContext, useContext, useState } from "react";
-import { Show, ShowsContextType } from "../utils/type";
+import { ReactNode, createContext, useContext, useEffect, useState } from "react";
+import { FavoriteProps, Show, ShowsContextType } from "../utils/type";
+import { supabase } from "../auth/supabase.service";
 
 const ShowsContext = createContext<ShowsContextType | null>(null)
 
@@ -8,10 +9,29 @@ export const ShowsContextProvider: React.FC<{children: ReactNode, initialShowLis
     const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
     const [token, setToken] = useState<string>('')
     const [selectedSeason, setSelectedSeason] = useState(1);
-    const [favourites, setFavourites] = useState([]);
+    const [favourites, setFavourites] = useState<FavoriteProps[] | null>(null);
 
     const [sort, setSort] = useState('');
     const [search, setSearch] = useState('');
+
+    useEffect(() => {
+      const fetchFavourites = async()=>{
+        const userId = await(await supabase.auth.getUser()).data.user?.id
+
+        const { data, error } = await supabase
+        .from('user_favourates')
+        .select("*")
+        .eq('userId', userId)
+
+        if(error) console.log(error.message)
+
+        setFavourites(data)
+      }
+
+
+      fetchFavourites()
+    }, [])
+    
    
    return (
     <ShowsContext.Provider value={{ shows, setShows, mobileMenuOpen, setMobileMenuOpen, token, setToken, sort, setSort, search, setSearch, selectedSeason, setSelectedSeason, 
